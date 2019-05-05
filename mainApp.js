fea71101e551d2ad3df7d051af3e98bd869983d5
Alessio Marinoni <alessio.marinoni1@gmail.com>
2019-05-05 23:48:16 +0200

    First commit

--- mainApp.js
+++ mainApp.js
@@ -3,37 +3,40 @@ const MainApp = {
   ctx: undefined,
   windowSize: {
     x: undefined,
-    y: undefined
+    y: undefined,
+    player: undefined,
+    fps: 60
   },
-  ball: undefined,
 
   init: function(id) {
-    console.log("helloo");
     this.canvasDom = document.getElementById(id);
     this.ctx = this.canvasDom.getContext("2d");
     this.setDimensions();
     this.setHandlers();
     this.setEventListeners();
+    // START GAME MUST BE DONE MANUALLY
+    this.startGame();
+  },
+
+  startGame: function() {
+    this.player = new Player(this.ctx, this.windowSize);
+    this.player.drawFilledSquares();
+    setInterval(() => {
+      this.clear();
+      this.player.drawFilledSquares();
+      // this.ball.draw();
+    }, 1000 / this.fps);
   },
 
   setDimensions: function() {
     this.canvasDom.setAttribute("width", window.innerWidth);
-    this.canvasDom.setAttribute("height", window.innerHeight);
+    this.canvasDom.setAttribute("height", window.innerHeight - 20);
     this.windowSize.y = window.innerHeight;
     this.windowSize.x = window.innerWidth;
   },
 
   setHandlers: function() {
     window.onresize = () => this.setDimensions();
-  },
-
-  drawControlledBall: function(url) {
-    this.ball = new Ball(this.ctx, url, this.winW);
-
-    setInterval(() => {
-      this.clear();
-      // this.ball.draw();
-    }, 5);
   },
 
   clear: function() {
@@ -47,5 +50,19 @@ const MainApp = {
     };
   }
 };
+
+class Player {
+  constructor(ctx, windowSize) {
+    this.ctx = ctx;
+    this.windowSize = windowSize;
+  }
+
+  drawFilledSquares() {
+    this.ctx.fillStyle = "blue"; // cambia los colores de relleno
+    this.ctx.fillRect(this.windowSize.x / 6, this.windowSize.y / 6, 100, 100);
+  }
+}
+
+class Boss {}
 
 MainApp.init("mycanvas");
